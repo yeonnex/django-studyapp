ac0a04c391a386c029ce995c59bcf3b82d82a0e0
yeonnex <yeonnex@gmail.com>
2021-08-29 10:49:37 +0900

    post 메서드로 내용 보낸 뒤 db에 저장된 내용 화면에 뿌려주기

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,8 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_output': new_hello_world})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': '안녕하세요! (get method)'})
 
